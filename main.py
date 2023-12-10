7c7c74e80e004452ae770b94023e6485bba188f5
anton <terentant@gmail.com>
2023-12-10 16:38:34 +0300

    changed field from random to glider and fixed generation problem

--- main.py
+++ main.py
@@ -7,15 +7,24 @@ GREY = (200, 200, 200)
 WHITE = (255, 255, 255)
 blockSize = 20
 WINDOW_HEIGHT = 400
-N_ROWS = 16
+N_ROWS = 9
 WINDOW_WIDTH = 400
-N_COLUMNS = 9
+N_COLUMNS = 16
 GENERATION = 0
+# random
+# C = [[random.choice([True, False])
+#             for _ in range(N_ROWS)]
+#            for _ in range(N_COLUMNS)]
 
-C = [[random.choice([True, False])
-            for _ in range(N_COLUMNS)]
-           for _ in range(N_ROWS)]
-CELLS = [C,C]
+C1 = [[False] * N_ROWS for _ in range(N_COLUMNS)]
+C2 = [[False] * N_ROWS for _ in range(N_COLUMNS)]
+C1[2][5] = True
+C1[3][3] = True
+C1[3][5] = True
+C1[4][4] = True
+C1[4][5] = True
+
+CELLS = [C1,  C2]
 # for i in range(16):
 #     for j in range(9):
 #         print(CELLS[i][j], end=' ')
@@ -26,7 +35,7 @@ def main():
     global SCREEN, CLOCK
     pg.init()
     SCREEN = pg.display.set_mode(
-        (N_ROWS * blockSize, N_COLUMNS * blockSize))
+        (N_COLUMNS * blockSize, N_ROWS * blockSize))
     CLOCK = pg.time.Clock()
     SCREEN.fill(WHITE)
 
@@ -42,29 +51,31 @@ def main():
 
 
 def drawGrid():
-    for x in range(N_ROWS):
-        for y in range(N_COLUMNS):
+    for x in range(N_COLUMNS):
+        for y in range(N_ROWS):
             rect = pg.Rect(x*blockSize, y*blockSize, blockSize, blockSize)
             pg.draw.rect(SCREEN, BLACK if CELLS[GENERATION][x][y] else WHITE, rect)
 
 
 def newGeneration():
-    for x in range(N_ROWS):
-        for y in range(N_COLUMNS):
-            neightbours = CELLS[GENERATION][x-1][y-1]+\
-                          CELLS[GENERATION][x][y-1]+ \
-                          CELLS[GENERATION][(x + 1)%N_ROWS][y - 1] + \
-                          CELLS[GENERATION][x-1][y] + \
-                          CELLS[GENERATION][(x + 1)%N_ROWS][y] + \
-                          CELLS[GENERATION][x - 1][(y + 1)%N_COLUMNS] + \
-                          CELLS[GENERATION][x][(y + 1)%N_COLUMNS] + \
-                          CELLS[GENERATION][(x + 1)%N_ROWS][(y + 1)%N_COLUMNS]
-
-            if neightbours == 3:
+    global GENERATION
+    for y in range(N_ROWS):
+        for x in range(N_COLUMNS):
+            neighbours = CELLS[GENERATION][(x - 1) % N_COLUMNS][(y - 1) % N_ROWS] \
+                         + CELLS[GENERATION][x % N_COLUMNS][(y - 1) % N_ROWS] \
+                         + CELLS[GENERATION][(x + 1) % N_COLUMNS][(y - 1) % N_ROWS] \
+                         + CELLS[GENERATION][(x - 1) % N_COLUMNS][y % N_ROWS] \
+                         + CELLS[GENERATION][(x + 1) % N_COLUMNS][y % N_ROWS] \
+                         + CELLS[GENERATION][(x - 1) % N_COLUMNS][(y + 1) % N_ROWS] \
+                         + CELLS[GENERATION][x % N_COLUMNS][(y + 1) % N_ROWS] \
+                         + CELLS[GENERATION][(x + 1) % N_COLUMNS][(y + 1) % N_ROWS]
+            if neighbours == 3:
                 CELLS[1 - GENERATION][x][y] = True
-            elif neightbours == 2 and CELLS[GENERATION][x][y]:
+            elif neighbours == 2 and CELLS[GENERATION][x][y]:
                 CELLS[1 - GENERATION][x][y] = True
             else:
                 CELLS[1 - GENERATION][x][y] = False
+    GENERATION = 1 - GENERATION
+
 
 main()
