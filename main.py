4ba74bbaa53ae8fb9bef01b64a4ca4ac88a0897b
anton <terentant@gmail.com>
2023-12-19 23:18:55 +0300

    added possibility to edit dimensions of cells field

--- main.py
+++ main.py
@@ -1,26 +1,23 @@
 import pygame as pg
 import tkinter as tk
-import sys
 import os
 import random
 
 BLACK = (0, 0, 0)
 GREY = (200, 200, 200)
 WHITE = (255, 255, 255)
-blockSize = 20
+blockSize = 16
 borderSize = blockSize // 10
-N_ROWS = 18
-N_COLUMNS = 32
+N_ROWS = 36
+N_COLUMNS = 64
 pg_height = N_ROWS * (blockSize + borderSize) - borderSize
 pg_width = N_COLUMNS * (blockSize + borderSize) - borderSize
 GENERATION = 0
 PLAY = True
 tick = 500
 
-C1 = [[False] * N_ROWS for _ in range(N_COLUMNS)]
-C2 = [[False] * N_ROWS for _ in range(N_COLUMNS)]
-
-CELLS = [C1,  C2]
+CELLS = [[[False] * N_ROWS for _ in range(N_COLUMNS)],
+         [[False] * N_ROWS for _ in range(N_COLUMNS)]]
 
 
 def coordinates(position):
@@ -50,11 +47,6 @@ def draw_grid():
 
 def new_generation():
     global GENERATION
-    # print('~' * 32)
-    # for y in range(N_ROWS):
-    #     for x in range(N_COLUMNS):
-    #         print(int(CELLS[GENERATION][x][y]), end=' ')
-    #     print()
     for y in range(N_ROWS):
         for x in range(N_COLUMNS):
             neighbours = CELLS[GENERATION][(x - 1) % N_COLUMNS][(y - 1) % N_ROWS] \
@@ -75,6 +67,7 @@ def new_generation():
 
 
 root = tk.Tk()
+root.title("Conway's Game of Life")
 embed_pygame = tk.Frame(root, width=pg_width, height=pg_height)
 embed_pygame.pack(side=tk.TOP)
 
@@ -83,6 +76,60 @@ os.environ['SDL_VIDEODRIVER'] = 'windib'
 pg.display.init()
 screen = pg.display.set_mode((pg_width, pg_height))
 screen.fill(WHITE)
+
+
+def set_dimensions(rows, columns, pixels):
+    global blockSize, borderSize, N_ROWS, N_COLUMNS, pg_height, \
+        pg_width, CELLS, screen, embed_pygame
+    N_ROWS = rows if rows > 10 else 10
+    N_COLUMNS = columns if columns > 10 else 10
+    blockSize = pixels if pixels > 4 else 4
+    borderSize = blockSize // 10 if blockSize >= 10 else 1
+    pg_height = N_ROWS * (blockSize + borderSize) - borderSize
+    pg_width = N_COLUMNS * (blockSize + borderSize) - borderSize
+    CELLS = [[[False] * N_ROWS for _ in range(N_COLUMNS)],
+             [[False] * N_ROWS for _ in range(N_COLUMNS)]]
+    embed_pygame.configure(width=pg_width, height=pg_height)
+
+
+def dimensions():
+    dimension_window = tk.Tk()
+    dimension_window.title("Change dimensions")
+
+    tk.Label(dimension_window, text=f'Now there are {N_ROWS} rows'
+                                    f', and  {N_COLUMNS} columns').pack(side=tk.TOP, anchor=tk.W)
+    tk.Label(dimension_window, text=f'With size of'
+                                    f' the cell {blockSize} pixels').pack(side=tk.TOP, anchor=tk.W)
+
+    rows_frame = tk.Frame(dimension_window)
+    rows_entry = tk.Entry(rows_frame)
+    rows_entry.insert(0, N_ROWS)
+    rows_entry.pack(side=tk.LEFT, anchor=tk.W)
+    tk.Label(rows_frame, text='Rows', padx=10).pack(side=tk.LEFT)
+    rows_frame.pack(side=tk.TOP, anchor=tk.W)
+
+    columns_frame = tk.Frame(dimension_window)
+    columns_entry = tk.Entry(columns_frame)
+    columns_entry.insert(0, N_COLUMNS)
+    columns_entry.pack(side=tk.LEFT, anchor=tk.W)
+    tk.Label(columns_frame, text='Columns', padx=10).pack(side=tk.LEFT)
+    columns_frame.pack(side=tk.TOP, anchor=tk.W)
+
+    pixels_frame = tk.Frame(dimension_window)
+    pixels_entry = tk.Entry(pixels_frame)
+    pixels_entry.insert(0, blockSize)
+    pixels_entry.pack(side=tk.LEFT, anchor=tk.W)
+    tk.Label(pixels_frame, text='Pixels for cell', padx=10).pack(side=tk.LEFT)
+    pixels_frame.pack(side=tk.TOP, anchor=tk.W)
+
+    def comma():
+        set_dimensions(int(rows_entry.get()),
+                       int(columns_entry.get()),
+                       int(pixels_entry.get()))
+        dimension_window.destroy()
+
+    change_button = tk.Button(dimension_window, text='Change!', command=comma)
+    change_button.pack(side=tk.TOP)
 
 
 def play_pause(menu):
@@ -129,7 +176,7 @@ def glider():
 main_menu = tk.Menu()
 
 window_menu = tk.Menu(tearoff=0)
-window_menu.add_command(label="Change dimensions")
+window_menu.add_command(label="Change dimensions", command=dimensions)
 window_menu.add_separator()
 window_menu.add_command(label="Random", command=random_field)
 window_menu.add_command(label="Blank", command=blank_field)
@@ -156,8 +203,6 @@ def pygame_loop():
     if PLAY:
         new_generation()
     for event in pg.event.get():
-        if event.type == pg.QUIT:
-            sys.exit()
         if event.type == pg.MOUSEBUTTONDOWN and event.button == 1:
             x, y = coordinates(event.pos)
             CELLS[GENERATION][x][y] = not CELLS[GENERATION][x][y]
